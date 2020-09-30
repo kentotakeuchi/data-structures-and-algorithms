6dd19597dbaf5d7b2ff432983b6557129180bd90
kentotakeuchi <ideal.kt.reality@gmail.com>
2020-09-29 20:17:15 -0700

    merge sort + reverse string

--- stephengrinder/reversestring/index.js
+++ stephengrinder/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let result = '';
+  for (let char of str) {
+    result = char + result;
+  }
+  return result;
+}
 
 module.exports = reverse;
--- stephengrinder/sorting/index.js
+++ stephengrinder/sorting/index.js
@@ -25,8 +25,24 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  const results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
