fc4cdc61e41c13b0f32132e501d04d4fba602f68
kentotakeuchi <ideal.kt.reality@gmail.com>
2020-10-01 21:47:02 -0700

    reverse int

--- stephengrinder/reverseint/index.js
+++ stephengrinder/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversedStr = Math.abs(n)
+    .toString()
+    .split('')
+    .reduce((acc, cur) => cur + acc, '');
+  return parseInt(reversedStr) * Math.sign(n); // Math.sign(number) --> return 1 or -1
+}
+
+// function reverseInt(n) {
+//   const isPositive = n >= 0;
+//   let reversedStr = Math.abs(n)
+//     .toString()
+//     .split('')
+//     .reduce((acc, cur) => cur + acc, '');
+//   if (!isPositive) reversedStr = '-' + reversedStr;
+//   return parseInt(reversedStr);
+// }
+
+reverseInt(123);
 
 module.exports = reverseInt;
