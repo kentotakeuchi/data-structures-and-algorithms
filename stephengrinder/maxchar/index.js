2d6a05daacae7d7964c4f631060c27ad61109312
kentotakeuchi <ideal.kt.reality@gmail.com>
2020-10-02 15:38:36 -0700

    max char

--- stephengrinder/maxchar/index.js
+++ stephengrinder/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  for (const char of str) {
+    obj[char] = obj[char] ? ++obj[char] : 1;
+  }
+
+  let max = 0;
+  let maxChar = '';
+  for (const key in obj) {
+    if (max < obj[key]) {
+      (max = obj[key]), (maxChar = key);
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
