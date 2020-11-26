e23e54ca034b3fb40b05d27e0d0f80d605a02ff1
kentotakeuchi <ideal.kt.reality@gmail.com>
2020-11-26 15:34:53 -0800

    decode-ways, bst > node class

---
+++ leetcode/medium/decode-ways.js
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/decode-ways/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// MINE
+// var numDecodings = function(s) {
+//     let result = 0;
+
+//     // find all set
+//     for(let i=0; i<s.length; i++) {
+
+//     }
+
+//     // check if decodable or not && increment count
+//         // deal with 0
+
+//     // return count
+// };
+
+// ANSWER
+// e.g. '226'
+// dp =
+// [1, 1, 0, 0]
+// [1, 1, 2, 0]
+// [1, 1, 2, 3]
+//
+// e.g. '236'
+// dp =
+// [1, 1, 0, 0]
+// [1, 1, 2, 0]
+// [1, 1, 2, 2]
+//
+// e.g. '101'
+// dp =
+// [1, 1, 0, 0]
+// [1, 1, 1, 0]
+// [1, 1, 1, 1]
+//
+// e.g. '110'
+// dp =
+// [1, 1, 0, 0]
+// [1, 1, 2, 0]
+// [1, 1, 2, 1]
+
+function numDecodings(s) {
+  if (s == null || s.length === 0) return 0;
+  if (s[0] === '0') return 0;
+
+  const dp = new Array(s.length + 1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= s.length; i++) {
+    const a = Number(s.slice(i - 1, i)); // last one digit
+    if (a >= 1 && a <= 9) {
+      dp[i] += dp[i - 1];
+    }
+
+    const b = Number(s.slice(i - 2, i)); // last two digits
+    if (b >= 10 && b <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  return dp[s.length];
+}
