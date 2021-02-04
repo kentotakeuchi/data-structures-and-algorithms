2086105307c515ac7a4365a27cbff6886c6efb07
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-02-03 23:53:02 -0800

    ll

--- stephengrinder/linkedlist/index.js
+++ stephengrinder/linkedlist/index.js
@@ -85,6 +85,21 @@ class LinkedList {
     }
     return node;
   }
+
+  removeAt(idx) {
+    if (!this.head) {
+      return;
+    }
+    if (idx === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const prev = this.getAt(idx - 1);
+    if (!prev || !prev.next) {
+      return;
+    }
+    prev.next = prev.next.next;
+  }
 }
 
 // ANSWER2
