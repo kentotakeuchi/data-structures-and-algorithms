42eb7667c23fd747b64a9a69b067a771ff9f26f1
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-02-10 14:35:02 -0800

    perfect-squares

---
+++ leetcode/medium/perfect-squares.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/perfect-squares/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// MINE..
+// var numSquares = function(n) {
+
+// };
+
+// ANSWER
+function numSquares(n) {
+  const dp = [0];
+
+  for (let i = 1; i <= n; i++) {
+    dp[i] = Number.MAX_VALUE;
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+
+  return dp[n];
+}
