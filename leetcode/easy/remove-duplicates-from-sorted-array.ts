b8bf7fa8c8ef40a27e54f5c66743a2ba416c518a
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-01 12:28:36 -0800

    remove-duplicates-from-sorted-array

---
+++ leetcode/easy/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+// MINE
+/*
+function removeDuplicates(nums: number[]): number {
+    // violate the constraints
+    // return [...new Set(nums)].length
+    
+    // O(n)
+    for(let i=1; i<nums.length; i++) {
+        if(nums[i-1] === nums[i]) {
+            nums.splice(i-1, 1)
+            --i
+            console.log({nums})
+        }
+    }
+    
+    return nums.length
+};
+*/
+
+// ANSWER: still duplicates remain
+/*
+function removeDuplicates(nums: number[]): number {
+    if (nums.length == 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+*/
+
+// ANOTHER ANSWER: remove duplicates from array
+var removeDuplicates = function (nums) {
+  if (nums.length < 2) return nums.length;
+
+  let i = nums.length - 2;
+
+  while (i >= 0) {
+    if (nums[i + 1] === nums[i]) {
+      nums.splice(i, 1);
+    }
+
+    i--;
+  }
+
+  return nums.length;
+};
