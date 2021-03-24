bf05859fd60dd3d5cf332581fdd416fcd9c00f7c
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-23 23:52:28 -0700

    validate

--- stephengrinder/validate/index.js
+++ stephengrinder/validate/index.js
@@ -5,18 +5,31 @@
 // every node's right hand child is greater than
 // the parent
 
+const Node = require('./node')
+
+/**
+ *
+ * @param {Node} node
+ * @param {(number|null)} min
+ * @param {(number|null)} max
+ * @return {boolean}
+ */
+
 // MINE
 function validate(node, min = null, max = null) {
   if (max && max < node.data) {
-    return false;
-  } else if (min && node.data < min) {
-    return false;
-  } else if (node.left && !validate(node.left, min, node.data)) {
-    return false;
-  } else if (node.right && !validate(node.right, node.data, max)) {
-    return false;
+    return false
   }
-  return true;
+  if (min && node.data < min) {
+    return false
+  }
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false
+  }
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false
+  }
+  return true
 }
 
 // ANSWER
@@ -28,4 +41,4 @@ function validate(node, min = null, max = null) {
 //   return true;
 // }
 
-module.exports = validate;
+module.exports = validate
