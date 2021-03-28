6143d4e730054d06b508ef2eceb673e237ad8a22
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-28 16:16:17 -0700

    bubble sort

--- stephengrinder/sorting/index.js
+++ stephengrinder/sorting/index.js
@@ -1,57 +1,39 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+/**
+ *
+ * @param {number[]} arr
+ * @param {number} i
+ * @param {number} j
+ */
+
 function swap(arr, i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]];
+  ;[arr[i], arr[j]] = [arr[j], arr[i]]
 }
+
+/**
+ *
+ * @param {number[]} arr
+ */
 
 // ANSWER
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1);
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j + 1] < arr[j]) {
+        swap(arr, j, j + 1)
       }
     }
   }
-  return arr;
+  return arr
 }
 
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let idxMin = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[idxMin]) {
-        idxMin = j;
-      }
-    }
-    if (i !== idxMin) {
-      swap(arr, i, idxMin);
-    }
-  }
-  return arr;
-}
+function selectionSort(arr) {}
 
-function mergeSort(arr) {
-  if (arr.length < 2) return arr;
-  const mid = Math.floor(arr.length / 2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-  return merge(mergeSort(left), mergeSort(right));
-}
+function mergeSort(arr) {}
 
-function merge(left, right) {
-  const results = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      results.push(left.shift());
-    } else {
-      results.push(right.shift());
-    }
-  }
-  console.log({ results, left, right });
-  return [...results, ...left, ...right];
-}
+function merge(left, right) {}
 
 // ANSWER
 // function bubbleSort(arr) {
@@ -98,4 +80,4 @@ function merge(left, right) {
 //   return [...results, ...left, ...right];
 // }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge }
