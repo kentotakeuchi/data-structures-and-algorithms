ef168d1e0ba66da8940e82e1bc2a5754b7873db4
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-29 14:31:43 -0700

    selection sort

--- stephengrinder/sorting/index.js
+++ stephengrinder/sorting/index.js
@@ -29,7 +29,27 @@ function bubbleSort(arr) {
   return arr
 }
 
-function selectionSort(arr) {}
+/**
+ *
+ * @param {number[]} arr
+ */
+
+function selectionSort(arr) {
+  let minIdx
+  for (let i = 0; i < arr.length - 1; ++i) {
+    minIdx = i
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j
+      }
+    }
+    if (i !== minIdx) {
+      swap(arr, i, minIdx)
+    }
+  }
+
+  return arr
+}
 
 function mergeSort(arr) {}
 
