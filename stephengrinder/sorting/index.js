06e9c435801fa6946c6651c29f13450e094db1d5
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-30 23:48:23 -0700

    merge sort

--- stephengrinder/sorting/index.js
+++ stephengrinder/sorting/index.js
@@ -51,9 +51,38 @@ function selectionSort(arr) {
   return arr
 }
 
-function mergeSort(arr) {}
+/**
+ *
+ * @param {number[]} arr
+ * @return {number[]}
+ */
 
-function merge(left, right) {}
+function mergeSort(arr) {
+  if (arr.length === 1) return arr
+  const mid = Math.floor(arr.length / 2)
+  const left = arr.slice(0, mid)
+  const right = arr.slice(mid)
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+/**
+ *
+ * @param {number[]} left
+ * @param {number[]} right
+ * @return {number[]}
+ */
+
+function merge(left, right) {
+  const results = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return [...results, ...left, ...right]
+}
 
 // ANSWER
 // function bubbleSort(arr) {
