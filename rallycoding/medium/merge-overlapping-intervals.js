11ad096785791e20fbddae4ab11b6f771c5eb380
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-01 14:16:44 -0700

    merge-overlapping-intervals

--- rallycoding/medium/merge-overlapping-intervals.js
+++ rallycoding/medium/merge-overlapping-intervals.js
@@ -22,16 +22,72 @@ const solve = (interval, newInterval) => {
 }
 */
 
+// messed up..
 /*
 const solve = (interval, newInterval) => {
-  for(let arr of interval) {
-    if(arr[1] < newInterval[0]) continue
-    if(newInterval[1] < arr[0]) continue
+  // base case
+  if(!interval.length) return []
+  
+  const results = []
+  let min = Infinity
+  let max = -Infinity
+  
+  for(let i=0; i<interval.length; ++i) {
+    // skip non overlap
+    if(interval[i][1] < newInterval[0] || newInterval[1] < interval[i][0]) {
+      results.push(interval[i])
+      continue
+    }
     
-    arr[0] = Math.min(arr[0], newInterval[0])
-    arr[1] = Math.max(arr[1], newInterval[1])
+    // 2,4, 5,7, 8,9
+    // 5, 12
+    min = Math.min(newInterval[0], interval[i][0]) // 5
+    max = Math.max(max, interval[i][1]) // 7
+    console.log({min, max})
+    
+    while(i < interval.length && max < newInterval[1]) {
+      ++i
+      
+      // skip non overlap
+      if(interval[i][1] < newInterval[0] || newInterval[1] < interval[i][0]) {
+        results.push(interval[i])
+        break
+      }
+      
+      max = Math.max(max, interval[i][1])
+    }
+    
+    // skip non overlap
+    if(interval[i][1] < newInterval[0] || newInterval[1] < interval[i][0]) {
+      continue
+    }
+    
+    results.push([min, max])
+    console.log({results})
   }
   
-  return interval;
+  return results
 };
 */
+
+// SOMEONE: worked but not good approach..
+const solve = (interval, newInterval) => {
+  const overlappingArray = [newInterval]
+  const result = []
+
+  interval.forEach((arr, i) => {
+    if (
+      (arr[0] >= newInterval[0] && arr[0] <= newInterval[1]) ||
+      (arr[1] >= newInterval[0] && arr[1] <= newInterval[1])
+    ) {
+      overlappingArray.push(interval[i])
+    } else {
+      result.push(interval[i])
+    }
+  })
+
+  const combinedArray = overlappingArray.flat().sort((a, b) => a - b)
+  const mergedArray = [combinedArray[0], combinedArray[combinedArray.length - 1]]
+
+  return [...result, mergedArray].sort((a, b) => a[0] + a[1] - (b[0] + b[1]))
+}
