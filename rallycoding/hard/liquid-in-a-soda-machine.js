25bdfaf1139aa285827674fc857edfe12aa93ca5
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-18 01:00:57 -0700

    wip: liquid-in-a-soda-machine

---
+++ rallycoding/hard/liquid-in-a-soda-machine.js
@@ -0,0 +1,16 @@
+// https://www.rallycoding.com/problems/168
+
+// MINE
+const solve = target => {
+  // [200, 300, 400]
+
+  let rest
+  let extra = 5
+
+  while (0 < target) {
+    if (400 <= target && target <= 400 + extra) {
+    }
+  }
+
+  return false
+}
