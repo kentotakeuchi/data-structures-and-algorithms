27098fd8f3bf4d2a98e3b7a0d0c744c0b53f1840
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-19 14:08:19 -0700

    wip: combinations-of-integers

---
+++ rallycoding/hard/combinations-of-integers.js
@@ -0,0 +1,14 @@
+// https://www.rallycoding.com/problems/181
+
+// MINE
+const solve = (arr, k) => {
+  const operators = ['+', '-', '*', '/']
+  for(let i=0; i<arr.length-1; ++i) {
+    for(let j=i+1; j<arr.length; ++j) {
+      for(let op of operators) {
+        if(arr[i])
+      }
+    }
+  }
+  return;
+};
