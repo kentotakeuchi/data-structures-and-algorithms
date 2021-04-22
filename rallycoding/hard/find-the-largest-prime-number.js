d00290544daa041063f4813531036a099f071e73
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-22 09:42:48 -0700

    find-the-largest-prime-number

--- rallycoding/hard/find-the-largest-prime-number.js
+++ rallycoding/hard/find-the-largest-prime-number.js
@@ -1,6 +1,8 @@
 // https://www.rallycoding.com/problems/191
 
-const isPrime = num => {
+// MINE
+/*
+ const isPrime = num => {
   for (let i = 2, s = Math.sqrt(num); i <= s; ++i) {
     if (num % i === 0) return false
   }
@@ -9,21 +11,21 @@ const isPrime = num => {
 
 const solve = intArray => {
   const arr = [...intArray]
-  console.log({ arr })
-
+  console.log({arr})
+  
   // store all sum combination into array
   for (let i = 0; i < intArray.length - 1; ++i) {
     let sum = intArray[i]
     for (let j = i + 1; j < intArray.length; ++j) {
       sum += intArray[j]
-      console.log({ sum }, intArray[i], intArray[j], arr)
+      console.log({sum}, intArray[i], intArray[j], arr)
       arr.push(sum)
     }
   }
 
   // sort sum array in descending order
   arr.sort((a, z) => z - a)
-  console.log({ arr })
+  console.log({arr})
 
   // check if each sum is prime number or not and find max one
   for (let sum of arr) {
@@ -32,4 +34,36 @@ const solve = intArray => {
 
   // if no prime number
   return -1
+}
+*/
+
+// SOMEONE
+const sums = xs => {
+  const found = []
+  function loop(acc, i) {
+    if (i >= xs.length) {
+      found.push(acc)
+    } else {
+      loop(acc, i + 1)
+      loop(acc + xs[i], i + 1)
+    }
+  }
+  loop(0, 0)
+  return found.filter(x => x > 1).sort((a, b) => b - a)
+}
+
+const solve = xs => {
+  const found = sums(xs)
+  const sieve = new Array(found[0] + 1).fill(0)
+  const markOff = v => {
+    if (sieve[v] === 0) {
+      for (let i = v + v; i < sieve.length; i += v) {
+        sieve[i] = 1
+      }
+    }
+  }
+  for (let i = 2; i < sieve.length; i++) {
+    markOff(i)
+  }
+  return found.find(x => sieve[x] === 0)
 }
