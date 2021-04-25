e5e2aedd18ee3004a41b5b829ee692bca995d986
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-24 22:12:55 -0700

    wip: binary-tree-paths

---
+++ leetcode/easy/binary-tree-paths.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/binary-tree-paths/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// MINE
+function binaryTreePaths(root: TreeNode | null): string[] {}
+
+// ANSWER
