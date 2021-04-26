437b66246e442de42aace0103c5a1d11ee9dd667
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-04-26 11:09:38 -0700

    binary-tree-paths

--- leetcode/easy/binary-tree-paths.ts
+++ leetcode/easy/binary-tree-paths.ts
@@ -14,8 +14,53 @@
  * }
  */
 
-// MINE
-function binaryTreePaths(root: TreeNode | null): string[] {}
+// MINE..
+/*
+function binaryTreePaths(root: TreeNode | null): string[] {
+    // base case
+    if(!root.left && !root.right) return [`${root.val}`]
+    
+    const arr = [root]
+    const results = []
+    let temps = []
+    
+    while(arr.length) {
+        const node = arr.shift()
+        temps.push(node.val)
+        
+        // if node is leaf
+        if(!node.left && !node.right) {
+            results.push(temps.join('->'))
+            temps = [root.val]
+        }
+        
+        if(node.left) {
+            arr.unshift(node.left)
+        }
+        if(node.right) {
+            arr.unshift(node.right)
+        }
+        console.log({arr, temps})
+    }
+    
+    return results
+};
+*/
 
-// ANSWER
-console.log()
+// ANSWER: recursion
+var binaryTreePaths = function (root: TreeNode | null): string[] {
+  let result: string[] = []
+  traverse(root, '')
+
+  function traverse(node: TreeNode | null, path: string) {
+    if (!node) return
+
+    if (!node.left && !node.right) {
+      result.push(path + node.val)
+      return
+    }
+    traverse(node.left, path + node.val + '->')
+    traverse(node.right, path + node.val + '->')
+  }
+  return result
+}
