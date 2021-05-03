3ada8b81532f575634448311a242200dd3390b8b
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-05-02 22:29:53 -0700

    wip: traversing-a-path-in-a-2d-array

---
+++ rallycoding/hard/traversing-a-path-in-a-2d-array.js
@@ -0,0 +1,16 @@
+// https://www.rallycoding.com/problems/246
+
+/**
+ *
+ * @param {number[][]} intArray
+ * @returns {string[]}
+ */
+
+// MINE
+const solve = intArray => {
+  // base case
+  if (!intArray.length) return ['']
+  if (intArray[0][0] === 0) return ['']
+
+  return
+}
