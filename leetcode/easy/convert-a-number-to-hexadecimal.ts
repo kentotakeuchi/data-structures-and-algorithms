6e0b9110aa9dc43d9b730ce78e46b7e949888791
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-05-19 13:46:02 -0700

    wip: convert-a-number-to-hexadecimal

---
+++ leetcode/easy/convert-a-number-to-hexadecimal.ts
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+
+// MINE..
+/*
+function toHex(num: number): string {
+
+};
+*/
+
+// ANSWER
+function toHex(num: number): string {
+  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+
+  if (num == 0) return '0'
+
+  if (num < 0) {
+    num += Math.pow(2, 32)
+  }
+  var res = ''
+
+  while (num > 0) {
+    var digit = num % 16
+    res = arr[digit] + res
+    num = Math.floor(num / 16)
+  }
+  return res
+}
