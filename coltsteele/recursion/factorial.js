fd9a60616948bd9dd297a912f094f7e6e9ee02e3
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-06-04 11:19:58 -0700

    factorial

---
+++ coltsteele/recursion/factorial.js
@@ -0,0 +1,26 @@
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+/**
+ *
+ * @param {number} num
+ * @returns {number}
+ */
+
+// MINE
+function factorial(num) {
+  if (num < 0) return 0
+  if (num <= 1) return 1
+  return num * factorial(--num)
+}
+
+// ANSWER
+function factorial(x) {
+  if (x < 0) return 0
+  if (x <= 1) return 1
+  return x * factorial(x - 1)
+}
+
+factorial(10)
