e37a5e3aaa509755e726b96eaf79dc4c76782ef2
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-06-09 14:26:25 -0700

    reverse

--- coltsteele/recursion/reverse.js
+++ coltsteele/recursion/reverse.js
@@ -5,7 +5,16 @@
  */
 
 // MINE
-function reverse(str) {}
+function reverse(str) {
+  if (str.length === 1) return str[0]
+  return str.slice(-1) + reverse(str.slice(0, str.length - 1))
+}
 
-reverse('awesome') // 'emosewa'
-reverse('rithmschool') // 'loohcsmhtir'
+// ANSWER
+function reverse(str) {
+  if (str.length <= 1) return str
+  return reverse(str.slice(1)) + str[0]
+}
+
+console.log(reverse('awesome')) // 'emosewa'
+console.log(reverse('rithmschool')) // 'loohcsmhtir'
