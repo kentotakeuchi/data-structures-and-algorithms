4b7ac2e87000ad2b852785e6a1d2f179eb8fc62f
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-06-19 19:00:46 -0700

    binary-search

---
+++ coltsteele/searching/binary-search.js
@@ -0,0 +1,71 @@
+/**
+ *
+ * @param {number[]} arr
+ * @param {number} val
+ * @returns {number}
+ */
+
+// MINE
+function binarySearch(arr, val) {
+  let start = 0
+  let end = arr.length - 1
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2)
+    if (val === arr[mid]) {
+      return mid
+    } else if (val < arr[mid]) {
+      end = mid - 1
+    } else {
+      start = mid + 1
+    }
+  }
+
+  return -1
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)) // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)) // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)) // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)) // -1
+console.log(
+  binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10)
+) // 2
+console.log(
+  binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95)
+) // 16
+console.log(
+  binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100)
+) // -1
+
+// Original Solution
+// function binarySearch(arr, elem) {
+//   var start = 0;
+//   var end = arr.length - 1;
+//   var middle = Math.floor((start + end) / 2);
+//   while(arr[middle] !== elem && start <= end) {
+//       if(elem < arr[middle]){
+//           end = middle - 1;
+//       } else {
+//           start = middle + 1;
+//       }
+//       middle = Math.floor((start + end) / 2);
+//   }
+//   if(arr[middle] === elem){
+//       return middle;
+//   }
+//   return -1;
+// }
+
+// // Refactored Version
+// function binarySearch(arr, elem) {
+//   var start = 0;
+//   var end = arr.length - 1;
+//   var middle = Math.floor((start + end) / 2);
+//   while(arr[middle] !== elem && start <= end) {
+//       if(elem < arr[middle]) end = middle - 1;
+//       else start = middle + 1;
+//       middle = Math.floor((start + end) / 2);
+//   }
+//   return arr[middle] === elem ? middle : -1;
+// }
