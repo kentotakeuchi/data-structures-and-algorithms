51c39084a153735b41f827b6734adfa573da67be
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-07-02 12:00:02 -0700

    wip: minimum-index-sum-of-two-lists

---
+++ leetcode/easy/minimum-index-sum-of-two-lists.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+
+// MINE
+function findRestaurant(list1: string[], list2: string[]): string[] {
+  const map = new Map()
+
+  for (let i = 0; i < list1.length; ++i) {
+    const idx = list2.find(list1[i])
+    map.set(list1[i], i + idx)
+  }
+}
+
+// ANSWER
