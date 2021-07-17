264985d13694de9581d1f1e5ec71380a9190f0bc
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-07-17 11:17:51 -0700

    longest-continuous-increasing-subsequence

---
+++ leetcode/easy/longest-continuous-increasing-subsequence.ts
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+
+// MINE
+/*
+function findLengthOfLCIS(nums: number[]): number {
+    let max = 1
+    let len = 1
+    
+    for(let i=1; i<nums.length; ++i) {        
+        if(nums[i-1] < nums[i]) {
+            ++len
+            max = Math.max(max, len)
+        } else {
+            len = 1
+        }
+    }
+    
+    return max
+};
+*/
+
+// ANSWER
+function findLengthOfLCIS(nums: number[]): number {
+  if (nums.length < 2) return nums.length
+  let left = 0,
+    right = 1,
+    maxLen = 0
+
+  while (right < nums.length) {
+    if (nums[right - 1] >= nums[right]) left = right
+    right++
+    maxLen = Math.max(right - left, maxLen)
+  }
+  return maxLen
+}
