1785556c8669b8235c59e82ef3cf4a1af9af580b
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-08-29 09:22:51 -0700

    wip: leaf-similar-trees

---
+++ leetcode/easy/leaf-similar-trees.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/leaf-similar-trees/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// MINE
+function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {}
+
+// ANSWER
