fdad3e72d0dbf735729cf2a837ccd970775a0d1f
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-09-26 23:00:57 -0700

    squares-of-a-sorted-array

--- leetcode/easy/squares-of-a-sorted-array.ts
+++ leetcode/easy/squares-of-a-sorted-array.ts
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/squares-of-a-sorted-array/
 
-// MINE
-function sortedSquares(nums: number[]): number[] {}
+// MINE..
+/*
+function sortedSquares(nums: number[]): number[] {
 
-// ANSWER
+};
+*/
+
+// ANSWER: two pointers
+function sortedSquares(nums: number[]): number[] {
+  const n = nums.length
+  const result: number[] = []
+  let i = 0
+  let j = n - 1
+
+  for (let p = n - 1; p >= 0; --p) {
+    if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+      result[p] = nums[i] * nums[i]
+      ++i
+    } else {
+      result[p] = nums[j] * nums[j]
+      --j
+    }
+  }
+
+  return result
+}
