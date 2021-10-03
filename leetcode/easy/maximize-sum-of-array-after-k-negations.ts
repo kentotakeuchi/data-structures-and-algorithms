0329c88bda3282e3812a7934cf8366cf7f97c567
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-10-03 12:15:44 -0700

    maximize-sum-of-array-after-k-negations

--- leetcode/easy/maximize-sum-of-array-after-k-negations.ts
+++ leetcode/easy/maximize-sum-of-array-after-k-negations.ts
@@ -1,6 +1,32 @@
 // https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
 
 // MINE
-function largestSumAfterKNegations(nums: number[], k: number): number {}
+/*
+function largestSumAfterKNegations(nums: number[], k: number): number {
+    while(k > 0){ 
+        let v = Math.min(...nums)
+        let i = nums.indexOf(v)
+        nums[i] = - nums[i]
+        k--
+    }
+    
+    return nums.reduce((acc,cur) => acc + cur, 0)
+};
+*/
 
 // ANSWER
+function largestSumAfterKNegations(nums: number[], k: number): number {
+  nums.sort((a, z) => a - z)
+
+  for (let i = 0; k > 0 && i < nums.length && nums[i] < 0; ++i, --k) nums[i] = -nums[i]
+
+  let res = 0
+  let min = Infinity
+
+  for (let num of nums) {
+    res += num
+    min = Math.min(min, num)
+  }
+
+  return res - (k % 2) * min * 2
+}
