684041cc9a32a9df76c729cf2e2cfc104c21ed61
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-10-18 23:35:40 -0700

    defanging-an-ip-address

--- leetcode/easy/defanging-an-ip-address.ts
+++ leetcode/easy/defanging-an-ip-address.ts
@@ -1,8 +1,23 @@
 // https://leetcode.com/problems/defanging-an-ip-address/
 
 // MINE
+/*
 function defangIPaddr(address: string): string {
   return address.replace(/[.]/g, '[.]')
-}
+};
+*/
 
 // ANSWER
+// function defangIPaddr(address: string): string {
+//     return address.replaceAll(".", "[.]");
+// }
+// function defangIPaddr(address: string): string {
+//     return address.split('.').join('[.]')
+// }
+function defangIPaddr(address: string): string {
+  let sb = ''
+  for (let c of address) {
+    sb += c === '.' ? '[.]' : c
+  }
+  return sb
+}
