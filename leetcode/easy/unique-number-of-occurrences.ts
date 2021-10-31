15ca0f71b84918746f4674b676385d9326e6409b
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-10-31 12:04:50 -0700

    unique-number-of-occurrences

--- leetcode/easy/unique-number-of-occurrences.ts
+++ leetcode/easy/unique-number-of-occurrences.ts
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/unique-number-of-occurrences/
 
 // MINE
-function uniqueOccurrences(arr: number[]): boolean {}
+function uniqueOccurrences(arr: number[]): boolean {
+  const map = new Map()
+  const set = new Set()
+
+  for (const n of arr) map.set(n, map.get(n) + 1 || 1)
+
+  for (const val of map.values()) {
+    if (set.has(val)) return false
+    else set.add(val)
+  }
+
+  return true
+}
 
 // ANSWER
