2d8049531bd7cca91e963110f8b0ff8a7c46bb70
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-11-15 14:12:39 -0800

    decrypt-string-from-alphabet-to-integer-mapping

--- leetcode/easy/decrypt-string-from-alphabet-to-integer-mapping.ts
+++ leetcode/easy/decrypt-string-from-alphabet-to-integer-mapping.ts
@@ -1,10 +1,61 @@
 // https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
 
-// MINE
-function freqAlphabets(s: string): string {
-  let ans = ''
-  
-  for(let i)
-};
+const map: { [key: string]: string } = {
+  '1': 'a',
+  '2': 'b',
+  '3': 'c',
+  '4': 'd',
+  '5': 'e',
+  '6': 'f',
+  '7': 'g',
+  '8': 'h',
+  '9': 'i',
+  '10': 'j',
+  '11': 'k',
+  '12': 'l',
+  '13': 'm',
+  '14': 'n',
+  '15': 'o',
+  '16': 'p',
+  '17': 'q',
+  '18': 'r',
+  '19': 's',
+  '20': 't',
+  '21': 'u',
+  '22': 'v',
+  '23': 'w',
+  '24': 'x',
+  '25': 'y',
+  '26': 'z'
+}
 
-// ANSWER
+// MINE
+/*
+function freqAlphabets(s: string): string {
+    let ans = ''
+    
+    for(let i=0; i<s.length; ++i) {
+        ans += map[s[i+2] === '#' ? (i+=2, s[i-2] + s[i-1]) : s[i]]
+    }
+    
+    return ans
+};
+*/
+
+// ANSWER: stack
+function freqAlphabets(s: string): string {
+  const stack = []
+  for (const char of s) {
+    if (char !== '#') {
+      stack.push(char)
+      continue
+    }
+    const digit = stack.pop()
+    stack.push(map[stack.pop()! + digit])
+  }
+  let ret = ''
+  for (const char of stack) {
+    ret += char <= '9' ? map[char] : char
+  }
+  return ret
+}
