1e7feadd115fbd05c1222423575b6a12a6d97fc4
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-11-17 11:12:13 -0800

    decompress-run-length-encoded-list

--- leetcode/easy/decompress-run-length-encoded-list.ts
+++ leetcode/easy/decompress-run-length-encoded-list.ts
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/decompress-run-length-encoded-list/
 
-// MINE
-function decompressRLElist(nums: number[]): number[] {}
+// MINE: naive
+/*
+function decompressRLElist(nums: number[]): number[] {
+    const ans = []
+    
+    for(let i=1; i<nums.length; i+=2) {
+        let freq = nums[i-1]
+        const val = nums[i]
+        
+        while(freq-- > 0) ans.push(val) 
+    }
+    
+    return ans
+};
+*/
 
 // ANSWER
+function decompressRLElist(nums: number[]): number[] {
+  const result = []
+
+  for (let i = 1; i < nums.length; i += 2) {
+    result.push(...new Array(nums[i - 1]).fill(nums[i]))
+  }
+
+  return result
+}
