b379abf7b34835ec971477469b0678d3dccd7a63
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-02-16 14:06:11 -0800

    decode-xored-array

---
+++ leetcode/easy/decode-xored-array.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/decode-xored-array/
+
+// MINE
+function decode(encoded: number[], first: number): number[] {
+  const ans = [first]
+
+  for (let i = 0; i < encoded.length; ++i) {
+    ans.push(encoded[i] ^ ans[i])
+  }
+
+  return ans
+}
+
+// ANSWER
