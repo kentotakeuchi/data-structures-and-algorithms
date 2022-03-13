9c2338de2db5dd7ce2ac1268435603800dc5a994
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-03-12 21:15:19 -0800

    minimum-operations-to-make-the-array-increasing

--- leetcode/easy/minimum-operations-to-make-the-array-increasing.ts
+++ leetcode/easy/minimum-operations-to-make-the-array-increasing.ts
@@ -1,8 +1,27 @@
 // https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
 
 // MINE
+/*
 function minOperations(nums: number[]): number {
-
+    let ans = 0
+    for(let i=1; i<nums.length; ++i) {
+        while(nums[i-1] >= nums[i]) {
+            ++nums[i]
+            ++ans
+        }
+    }
+    return ans
 };
+*/
 
 // ANSWER
+function minOperations(nums: number[]): number {
+    let ans = 0
+    for(let i=1; i<nums.length; ++i) {
+        if(nums[i-1] >= nums[i]) {
+            ans = ans + nums[i-1] - nums[i]+1
+            nums[i] = nums[i-1]+1
+        }                      
+    }
+    return ans
+}
