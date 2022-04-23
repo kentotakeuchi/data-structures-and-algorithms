0ef0b2c5fe2681c3cf60fd9cc4ca25ea8009f727
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-04-23 16:39:33 -0700

    minimum-number-of-moves-to-seat-everyone

---
+++ leetcode/easy/minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        return sum(abs(e - t) for e, t in zip(seats, students))
