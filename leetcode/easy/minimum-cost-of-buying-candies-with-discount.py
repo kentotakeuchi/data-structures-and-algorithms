bddf1796b6ad9ac7222ee7fd9ddc8d1ad0b74972
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-05-15 18:31:14 -0700

    minimum-cost-of-buying-candies-with-discount

---
+++ leetcode/easy/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse=True)
+        ans = 0
+        for i in range(len(cost)):
+            if (i+1) % 3 != 0:
+                ans += cost[i]
+        return ans
