91e60b07f52c603e0da5203cde66785c88864658
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-06-27 23:14:19 -0700

    calculate-amount-paid-in-taxes

--- leetcode/easy/calculate-amount-paid-in-taxes.ts
+++ leetcode/easy/calculate-amount-paid-in-taxes.ts
@@ -1,1 +1,20 @@
 //
+// https://leetcode.com/problems/calculate-amount-paid-in-taxes/
+
+// MINE
+function calculateTax(brackets: number[][], income: number): number {
+  let ans = 0,
+    prev = 0
+  for (let [upper, percent] of brackets) {
+    if (upper <= income) {
+      ans += ((upper - prev) * percent) / 100
+      prev = upper
+    } else {
+      ans += ((income - prev) * percent) / 100
+      return ans
+    }
+  }
+  return ans
+}
+
+// ANSWER
