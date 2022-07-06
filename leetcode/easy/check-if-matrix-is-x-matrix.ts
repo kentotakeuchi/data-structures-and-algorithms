212f20aaca00172244b19cf3f8a604def37a430e
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-07-06 14:15:46 -0700

    done: check-if-matrix-is-x-matrix

--- leetcode/easy/check-if-matrix-is-x-matrix.ts
+++ leetcode/easy/check-if-matrix-is-x-matrix.ts
@@ -1,1 +1,18 @@
-// wip
+// https://leetcode.com/problems/check-if-matrix-is-x-matrix/
+
+// MINE
+function checkXMatrix(grid: number[][]): boolean {
+  const n = grid.length
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < n; ++j) {
+      if (i === j || i === n - j - 1) {
+        if (!grid[i][j]) return false
+      } else {
+        if (grid[i][j]) return false
+      }
+    }
+  }
+  return true
+}
+
+// ANSWER
