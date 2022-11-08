341dd80085f5069ae3180df8eea4ae172eea55dd
kentotakeuchi <ideal.kt.reality@gmail.com>
2022-11-08 13:19:22 -0800

    done: valid-parentheses

--- leetcode/easy/valid-parentheses.ts
+++ leetcode/easy/valid-parentheses.ts
@@ -6,27 +6,69 @@
 // };
 
 // ANSWER: stack + mirror
-var isValid = function (s: string): boolean {
-  const stack = [];
+// var isValid = function (s: string): boolean {
+//   const stack = [];
 
-  for (let i = 0; i < s.length; i++) {
-    let c = s.charAt(i);
+//   for (let i = 0; i < s.length; i++) {
+//     let c = s.charAt(i);
+//     switch (c) {
+//       case '(':
+//         stack.push(')');
+//         break;
+//       case '[':
+//         stack.push(']');
+//         break;
+//       case '{':
+//         stack.push('}');
+//         break;
+//       default:
+//         if (c !== stack.pop()) {
+//           return false;
+//         }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
+
+// mine..
+// function isValid(s: string): boolean {
+//     const map = {
+//         '(': true,
+//         '{': true,
+//         '[': true,
+//     }
+//     for(let c of s) {
+//         if(c === '(' || c === '{' || c === '[') map[c] = false
+//         else if(map['('] && c === ')' || map['{'] && c === '}' || map['['] && c === ']') map[c] = false
+//         else if(c === ')') map['('] = true
+//         else if(c === '}') map['{'] = true
+//         else map['['] = true
+//         console.log({map})
+//     }
+//     for(let k in map) {
+//         if(!map[k]) return false
+//     }
+//     return true
+// };
+
+// ans: stack
+function isValid(s: string): boolean {
+  const stack = []
+  for (let c of s) {
     switch (c) {
       case '(':
-        stack.push(')');
-        break;
-      case '[':
-        stack.push(']');
-        break;
+        stack.push(')')
+        break
       case '{':
-        stack.push('}');
-        break;
+        stack.push('}')
+        break
+      case '[':
+        stack.push(']')
+        break
       default:
-        if (c !== stack.pop()) {
-          return false;
-        }
+        if (c !== stack.pop()) return false
     }
   }
-
-  return stack.length === 0;
-};
+  return !stack.length
+}
