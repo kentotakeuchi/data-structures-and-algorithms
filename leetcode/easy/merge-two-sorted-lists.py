2eca8e934b3adfc177c670593b5e93878a359ea4
kentotakeuchi <ideal.kt.reality@gmail.com>
2023-01-10 13:03:34 -0800

    merge-two-sorted-lists

---
+++ leetcode/easy/merge-two-sorted-lists.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# mine try1
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        while l1 and l2:
+            if l1.val > l2.val:
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return dummy.next
--- leetcode/easy/merge-two-sorted-lists.ts
+++ leetcode/easy/merge-two-sorted-lists.ts
@@ -34,20 +34,41 @@
 // };
 
 // ANSWER
-var mergeTwoLists = function (l1, l2) {
-  var mergedHead = { val: -1, next: null },
-    crt = mergedHead;
-  while (l1 && l2) {
-    if (l1.val > l2.val) {
-      crt.next = l2;
-      l2 = l2.next;
-    } else {
-      crt.next = l1;
-      l1 = l1.next;
-    }
-    crt = crt.next;
-  }
-  crt.next = l1 || l2;
+// var mergeTwoLists = function (l1, l2) {
+//   var mergedHead = { val: -1, next: null },
+//     crt = mergedHead;
+//   while (l1 && l2) {
+//     if (l1.val > l2.val) {
+//       crt.next = l2;
+//       l2 = l2.next;
+//     } else {
+//       crt.next = l1;
+//       l1 = l1.next;
+//     }
+//     crt = crt.next;
+//   }
+//   crt.next = l1 || l2;
 
-  return mergedHead.next;
-};
+//   return mergedHead.next;
+// };
+
+// try2
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  let dummy: ListNode = {
+    val: -1,
+    next: null
+  }
+  let cur = dummy
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      cur.next = list2
+      list2 = list2.next
+    } else {
+      cur.next = list1
+      list1 = list1.next
+    }
+    cur = cur.next
+  }
+  cur.next = list1 || list2
+  return dummy.next
+}
