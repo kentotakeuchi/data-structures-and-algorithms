a1e4dcae66d2eaafee9d601e82438a7eff32fbdf
kentotakeuchi <ideal.kt.reality@gmail.com>
2023-01-24 14:07:17 -0800

    climbing-stairs

--- leetcode/easy/climbing-stairs.ts
+++ leetcode/easy/climbing-stairs.ts
@@ -1,3 +1,5 @@
+'use strict'
+
 // https://leetcode.com/problems/climbing-stairs/
 
 // MINE..
@@ -48,13 +50,38 @@ const fast = memoize(_climbStairs)
 */
 
 // ANSWER: dynamic programming
+// function climbStairs(n: number): number {
+//   if (n === 1) return 1
+
+//   const dp: number[] = [0, 1, 2]
+//   for (let i = 3; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2]
+//   }
+
+//   return dp[n]
+// }
+
+// try2.. need to work on ts stuff..
+type fnType = (args: number[]) => number
+
 function climbStairs(n: number): number {
-  if (n === 1) return 1
-
-  const dp: number[] = [0, 1, 2]
-  for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2]
-  }
-
-  return dp[n]
+  return _climbStairs(0, n)
 }
+
+function _climbStairs(i: number, steps: number): number {
+  if (steps < i) return 0
+  if (steps === i) return 1
+  return fast(i + 1, steps) + fast(i + 2, steps)
+}
+
+;(function memo(fn?: fnType): number {
+  const cache: { [key: string]: number } = {}
+  return function (...args: number[]): number {
+    if (cache[`${args}`]) return cache[`${args}`]
+    const ans = fn.apply(this, args)
+    cache[`${args}`] = ans
+    return ans
+  }
+})()
+
+const fast = memo(_climbStairs)
